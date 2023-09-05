6cf9f853294133942fdf9ca4d47c62afd5eb7201
october03 <season_autumn@kakao.com>
2023-09-05 18:55:51 +0900

    ApiService 생성

---
+++ src/service/ApiService.ts
@@ -0,0 +1,10 @@
+import axios from "../../node_modules/axios/index";
+import "dotenv/config";
+
+const ApiService = axios.create({
+	baseURL: "https://api.twitch.tv/helix",
+	headers: {
+		Authorization: `Bearer ${process.env.API_TOKEN}`,
+		"Client-Id": process.env.API_CLIENT_ID,
+	},
+});
